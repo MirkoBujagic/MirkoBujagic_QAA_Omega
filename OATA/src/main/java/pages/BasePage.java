0e4af8d2494bec7c5d1913e5110ddc619e4216ec
Mirko Bujagic <mirko.bujagic@vegait.rs>
2023-09-27 15:15:14 +0200

    Some commit

--- OATA/src/main/java/pages/BasePage.java
+++ OATA/src/main/java/pages/BasePage.java
@@ -4,6 +4,7 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.WebDriverWait;
+
 import java.time.Duration;
 
 public class BasePage {
@@ -15,23 +16,24 @@ public class BasePage {
         this.driver = driver;
         wait = new WebDriverWait(driver, Duration.ofSeconds(30));
     }
+
     //wait for element to be visible
-    public void waitVisibility(WebElement element){
+    public void waitVisibility(WebElement element) {
         wait.until(ExpectedConditions.visibilityOfAllElements(element));
     }
+
     //wait for element and click on it
-    public void click(WebElement element){
+    public void click(WebElement element) {
         waitVisibility(element);
         element.click();
     }
+
     //wait for element, clear the field and write text
-    public void writeText(WebElement element, String text){
+    public void writeText(WebElement element, String text) {
         waitVisibility(element);
         element.clear();
         element.sendKeys(text);
     }
-
-
 
 
 }
